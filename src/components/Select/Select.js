72c683e42d83ace0e85f41ea06eca523e0b8a337
Renata <redzh@smart-it.com>
2023-01-09 13:10:03 +0200

    Create Select component

--- src/components/Select/Select.js
+++ src/components/Select/Select.js
@@ -1,18 +1,51 @@
-import React from 'react';
-import styled from 'styled-components';
+import React from "react";
+import styled from "styled-components";
 
-import { COLORS } from '../../constants';
-import Icon from '../Icon';
-import { getDisplayedValue } from './Select.helpers';
+import { COLORS } from "../../constants";
+import Icon from "../Icon";
+import { getDisplayedValue } from "./Select.helpers";
 
 const Select = ({ label, value, onChange, children }) => {
   const displayedValue = getDisplayedValue(value, children);
 
   return (
-    <select value={value} onChange={onChange}>
-      {children}
-    </select>
+    <Wrapper>
+      <NativeSelect value={value} onChange={onChange}>
+        {children}
+      </NativeSelect>
+      <IconWrapper>
+        <Icon id="chevron-down" strokeWidth={1} size={24} />
+      </IconWrapper>
+    </Wrapper>
   );
 };
+
+const Wrapper = styled.div`
+  position: relative;
+  width: max-content;
+  cursor: pointer;
+`;
+
+const IconWrapper = styled.div`
+  position: absolute;
+  right: 14px;
+  top: 8px;
+  pointer-events: none;
+`;
+
+const NativeSelect = styled.select`
+  appearance: none;
+  width: auto;
+  background-color: ${COLORS.transparentGray15};
+  border-radius: 8px;
+  border: none;
+  color: ${COLORS.gray700};
+
+  padding: 12px 52px 12px 16px;
+
+  div {
+    width: fit-content;
+  }
+`;
 
 export default Select;
